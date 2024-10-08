8d06d445eca1e4782e7fefaed09bbab2fb8a0cc8
Subhadip Sahoo <subhadip.developer@gmail.com>
2024-10-08 20:24:53 +0530

    Updated add() passing test cases #4 and #5

--- src/add.ts
+++ src/add.ts
@@ -8,9 +8,9 @@ export const add = function(numbers: string): number {
   if (numbers === "") return 0; // Test case # 1
 
   let numStr = numbers;
-  let deliminator = ',';
+  let deliminator = /,|\n/;
 
   const numArr = numStr.split(deliminator);
 
-  return numArr.reduce((sum, num) => sum + +num, 0); // Test case # 2, 3
+  return numArr.reduce((sum, num) => sum + +num, 0);
 };
