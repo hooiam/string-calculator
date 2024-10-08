3e5316354eb0b80014f302513cac90916113aafe
Subhadip Sahoo <subhadip.developer@gmail.com>
2024-10-08 19:17:14 +0530

    Added few test cases

--- src/test/add.test.ts
+++ src/test/add.test.ts
@@ -1,3 +1,17 @@
 import { add } from '../add';
 
-// TODO: Test cases for all possible use cases
+describe('String Calculator', () => {
+
+  test('should return 0 for an empty string', () => {
+      expect(add("")).toBe(0);
+  });
+
+  test('should return the number itself when there is only one number', () => {
+      expect(add("1")).toBe(1);
+  });
+
+  test('should return the sum of two comma-separated numbers', () => {
+      expect(add("1,5")).toBe(6);
+  });
+
+});
