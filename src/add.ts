a2d07344ab9c4b0665af3214f6fc95b21d39b1bc
Subhadip Sahoo <subhadip.developer@gmail.com>
2024-10-08 19:46:33 +0530

    Updated add function to pass test case #1, #2, #3

--- src/add.ts
+++ src/add.ts
@@ -1,11 +1,16 @@
 /**
  * Function to calculate sum of all postitive numbers passed as string separated by deliminator
- * @param {String} number 
+ * @param {String} numbers 
  * @returns {Number} 
  */
 
-export const add = function(number: string): number {
-  let result = 0;
-  // TODO: String parsing, validation, exception handling and calculate final result
-  return result;
+export const add = function(numbers: string): number {
+  if (numbers === "") return 0; // Test case # 1
+
+  let numStr = numbers;
+  let deliminator = ',';
+
+  const numArr = numStr.split(deliminator);
+
+  return numArr.reduce((sum, num) => sum + +num, 0); // Test case # 2, 3
 };
--- src/test/add.test.ts
+++ src/test/add.test.ts
@@ -2,16 +2,16 @@ import { add } from '../add';
 
 describe('String Calculator', () => {
 
-  test('should return 0 for an empty string', () => {
+  test('#1 should return 0 for an empty string', () => {
       expect(add("")).toBe(0);
   });
 
-  test('should return the number itself when there is only one number', () => {
+  test('#2 should return the number itself when there is only one number', () => {
       expect(add("1")).toBe(1);
   });
 
-  test('should return the sum of two comma-separated numbers', () => {
+  test('#3 should return the sum of two comma-separated numbers', () => {
       expect(add("1,5")).toBe(6);
   });
-
+  
 });
